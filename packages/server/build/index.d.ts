6a1acc02b927d17b3b954d57231060f37ed34c4c
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-08-21 09:57:55 +0300

    Add 'build' folders to gitignore

--- packages/server/build/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const express: any;
-declare const cors: any;
-declare const cookieParser: any;
-declare const PORT: string | number;
-declare const app: any;
-declare const run: () => Promise<void>;
