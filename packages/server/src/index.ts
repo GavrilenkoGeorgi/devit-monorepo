27afb8e3e6135dcbcdd15b8be7d92700c6143a5c
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-08-21 10:01:18 +0300

    Connect db

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+// const cors = require('cors')
+import cookieParser from 'cookie-parser'
+
+/* eslint-disable-next-line */
 require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+
 const run = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL || '')
+
     app.listen(PORT, () => {
       console.log('Server started on port:', PORT)
     })
