e0383cb16f025c39403033969865ce2070f2b369
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-08-25 13:59:04 +0300

    Cleanup unused packages

--- packages/server/src/models/rss-item-model.ts
+++ packages/server/src/models/rss-item-model.ts
@@ -1,12 +1,9 @@
 import { Schema, model }  from 'mongoose'
-import mongoosePaginate from 'mongoose-paginate'
 
 export const RssItemSchema = new Schema({
   title: String,
   link: String,
   pubDate: String
 })
-
-RssItemSchema.plugin(mongoosePaginate)
 
 export default model('RssItem', RssItemSchema)
